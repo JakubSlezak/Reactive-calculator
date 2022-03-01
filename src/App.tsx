a4be23d9a2c8e6e8e009a161dc8b37f24b44aafa
Jakub Ślęzak <jakub.slezak@protonmail.com>
2022-03-01 15:50:15 +0100

    Handling buttons control

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import { Button } from './components/Button/Button';
 
 function App() {
+    const [state, setState] = useState('0');
+    const list = ['C', '0', '.', '=', '1', '2', '3', '/', '4', '5', '6', '*', '7', '8', '9', '-', 'sqrt', 'pow', '%', '+'];
     return (
         <div className="App">
             <header className="App-header">
                 <span>REACTive Calculator</span>
                 <img src={logo} className="App-logo" alt="logo" />
+                <span>{state}</span>
                 <div className='row'>
-                    <Button value='sqrt' />
-                    <Button value='pow' />
-                    <Button value='%' />
-                    <Button value='+' />
-                </div>
-                <div className='row'>
-                    <Button value='7' />
-                    <Button value='8' />
-                    <Button value='9' />
-                    <Button value='-' />
-                </div>
-                <div className='row'>
-                    <Button value='4' />
-                    <Button value='5' />
-                    <Button value='6' />
-                    <Button value='*' />
-                </div>
-                <div className='row'>
-                    <Button value='1' />
-                    <Button value='2' />
-                    <Button value='3' />
-                    <Button value='/' />
-                </div>
-                <div className='row'>
-                    <Button value='C' />
-                    <Button value='0' />
-                    <Button value='.' />
-                    <Button value='=' />
+                    {list.map((item) => (<Button onClickHandler={(item) => setState(item)} value={item} />))}
                 </div>
             </header>
         </div>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,10 +6,12 @@ import { ButtonProps } from './ButtonProps';
 //styles
 import css from './Button.module.scss';
 
-export const Button: React.FC<ButtonProps> = ({value}) => {
+export const Button: React.FC<ButtonProps> = (props) => {
+    const { value, onClickHandler = () => {} } = props;
+
     return (
         <div className={css.buttonWrapper}>
-            <button className={css.buttonValue}>{value}</button>
+            <button className={css.buttonValue} onClick={() => onClickHandler(value)}>{value}</button>
         </div>
     );
 };
